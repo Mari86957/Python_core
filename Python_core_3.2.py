38a79428ee479f5260447185573ef9c8d377bbb2
Mari86957 <133572710+Mari86957@users.noreply.github.com>
2024-07-19 21:09:27 +0300

    Add files via upload

--- Python_core_3.2.py
+++ Python_core_3.2.py
@@ -1,6 +1,6 @@
 from random import randint, sample
-
-min = 1
-max = 500
-quantity = 5
-print(sorted(sample(range(min, max), quantity)))
+def get_numbers_ticket(min, max, quantity): 
+    min = 1
+    max = 500
+    quantity = 5   
+    return(sorted(sample(range(min, max), quantity)))
--- Python_core_3.py
+++ Python_core_3.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-# def get_days_from_today():
-date = input("Введіть дату в форматі:'YYY-MM-DD': ")
-day = datetime.strptime(date, '%Y-%m-%d').date()
-# print(day)
 
-today_date = datetime.today().date()
-# print(today_date)
+def get_days_from_today():
+    date = input("Введіть дату в форматі YYY-MM-DD: ")
+    user_date = datetime.strptime(date,  '%Y-%m-%d').date()
+    # print(user_date)
 
-delta_days = today_date - day
-print(delta_days)
+    today_date = datetime.today().date()
+    # print(today_date)
+
+    delta_days = today_date - user_date
+    return(delta_days)
