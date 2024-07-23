082f1084f19d4216e4b760988fd1c75fea632073
Mari86957 <133572710+Mari86957@users.noreply.github.com>
2024-07-23 11:07:53 +0300

    Add files via upload

--- Python_core_3.2.py
+++ Python_core_3.2.py
@@ -3,7 +3,7 @@ def get_numbers_ticket(min, max, quantity):
     try:
         return sorted(sample(range(min, max), quantity))
     except ValueError:
-        return ""
+        return []
 print(get_numbers_ticket(10,20,5))
 print(get_numbers_ticket(10,15,5))
 print(get_numbers_ticket(10,4,5))
